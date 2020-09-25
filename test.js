c10a490a9398de054cafd865997abd014360c463
Ana Lam <alam@barnard.edu>
2020-09-25 12:42:08 -0400

    re add jekyll-pdf gem

--- test.js
+++
@@ -1,6 +0,0 @@
-var markdownpdf = require("markdown-pdf")
-  , fs = require("fs")
- 
-fs.createReadStream("test.md")
-  .pipe(markdownpdf())
-  .pipe(fs.createWriteStream("test.pdf"))
--- test.md
+++
@@ -1,2 +0,0 @@
-# Hello World
-## this is al3574
